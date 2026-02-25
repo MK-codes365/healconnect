dac26c578963a76fad05a77cbb7164aafa3fb683
Mukut Kumar <mukutkumar842@gmail.com>
2026-02-25 21:21:43 +0530

    Update YouTube video source in DemoVideo component

--- src/components/DemoVideo.jsx
+++ src/components/DemoVideo.jsx
@@ -13,7 +13,7 @@ const DemoVideo = () => {
                     <iframe 
                         width="100%" 
                         height="500" 
-                        src="https://www.youtube.com/embed/psgTf_T03Tc" 
+                        src="https://www.youtube.com/watch?v=72cOK8R02cI" 
                         title="HealConnect Demo Video" 
                         frameBorder="0" 
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
